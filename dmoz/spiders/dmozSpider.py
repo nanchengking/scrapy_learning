68da33c081c7bf939210d4e66f6a7dba4c18edab
刘述清 <nancheng@outlook.com>
2015-08-24 21:49:52 +0800

    测试通过，被空格坑了！！！

--- dmoz/spiders/dmozSpider.py
+++ dmoz/spiders/dmozSpider.py
@@ -1,1 +1,21 @@
 #coding=utf-8
+import scrapy
+class w3cSpider(scrapy.Spider):
+	name="dmoz"
+	allowed_domains = ["taobao.com/"]
+	start_urls = [
+       "https://www.taobao.com/"
+    ] 
+
+	def parse(self,response):
+		self.num=0
+		self.wanted_num=10
+		for sel in response.xpath("//a[@href]/@href"):
+			self.num=self.num+1
+			if(self.num>=self.wanted_num):
+				return
+			print sel.extract()
+
+
+
+
